73ec23c6432c01d6b5d1825c0464ae4fb74d073a
Justin Z <justinwzig@gmail.com>
2018-02-07 13:26:32 -0600

    More TVDQI

--- 6. ordinal-data/index.js
+++ 6. ordinal-data/index.js
@@ -1,37 +1,38 @@
-var width = 500,
-    height = 300;
+var width = 960,
+    height = 500;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
 
 var y = d3.scale.linear()
     .range([height, 0]);
 
 var chart = d3.select(".chart")
-    .attr("height", height)
-    .attr("width", width);
+    .attr("width", width)
+    .attr("height", height);
 
-d3.csv("data.csv", processDataType, function (error, data) {
-
-    y.domain([0, d3.max(data, function (d) { return d.value })])
-
-    var barWidth = width / data.length;
+d3.csv("data.csv", type, function (error, data) {
+    x.domain(data.map(function (d) { return d.name; }));
+    y.domain([0, d3.max(data, function (d) { return d.value; })]);
 
     var bar = chart.selectAll("g")
         .data(data)
         .enter().append("g")
-        .attr("transform", function (data, index) { return "translate(" + index * barWidth + ",0)"; });
+        .attr("transform", function (d) { return "translate(" + x(d.name) + ",0)"; });
 
     bar.append("rect")
         .attr("y", function (d) { return y(d.value); })
         .attr("height", function (d) { return height - y(d.value); })
-        .attr("width", barWidth - 1);
+        .attr("width", x.rangeBand());
 
     bar.append("text")
-        .attr("x", barWidth / 2)
-        .attr("y", function (d) { return y(d.value) - 3; })
-        .attr("dy", ".35em")
-        .text(function (d) { return d.value });
-})
+        .attr("x", x.rangeBand() / 2)
+        .attr("y", function (d) { return y(d.value) + 3; })
+        .attr("dy", ".75em")
+        .text(function (d) { return d.value; });
+});
 
-function processDataType(d) {
-    d.value = +d.value; //grabbing the VALUE
+function type(d) {
+    d.value = +d.value; // coerce to number
     return d;
 }
--- TVDQI/7/index.js
+++ TVDQI/7/index.js
@@ -1,39 +1,65 @@
-var width = 500,
-    height = 400;
+//define your outer width and outer height, then your margins.
+var outerWidth = 900
+    outerHeight= 500
+    margin = { top: 20, right: 20, bottom: 20, left: 50 },
+//these are computed automatically
+    width = outerWidth - margin.left - margin.right,
+    height = outerHeight - margin.top - margin.bottom;
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+var xScale = d3.scale.linear()
+    .range([0,width])
+//this defines a linear scale
+var yScale = d3.scale.linear()
+    //.domain([0,d3.max(data.value)])
+    .range([height,0])
 
 var chart = d3.select(".chart")
-    .attr("height", height)
-    .attr("width", width);
+    .attr("width", outerWidth)
+    .attr("height", outerHeight)
+    .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("data.csv", processDataType, function (error, data) {
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
 
-    y.domain([0, d3.max(data, function (d) { return d.value })])
+var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
 
+
+d3.csv("data.csv", processDataType, function(error,data){
+    //x scale from above
+    xScale.domain([0,d3.max(data, function(d){return d.x})])
+    //y scale from above
+    yScale.domain([d3.min(data,function(d){return d.y}),d3.max(data, function(d){return d.y})])
+
+    //axes
+//    chart.append("g")
+//        .attr("class", "x axis")
+//        .attr("transform", "translate(0,"+height+")")
+//        .call(xAxis);
+    chart.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+
+    //items
     var itemWidth = width / data.length;
-    var translateX = index*itemWidth+itemWidth/2;
 
-    var item = chart.selectAll("g")
+    var item = chart.selectAll("bar")
         .data(data)
         .enter().append("g")
-        .attr("transform", function (data, index) { return "translate(" + translateX + ",0)"; });
-
-    /*bar.append("rect")
-        .attr("y", function (d) { return y(d.value); })
-        .attr("height", function (d) { return height - y(d.value); })
-        .attr("width", barWidth - 1);
-    */
+        .attr("transform", function (dataObject, index) { return "translate(" + index * itemWidth + ",0)"; });
 
     item.append("text")
-        .attr("x", itemWidth/2)
-        .attr("y", function (d) { return y(d.value); })
-        .attr("dy", ".35em")
-        .text(function (d) { return d.value });
+        .attr("x", itemWidth)
+        .attr("y", function(d) {return yScale(d.y);})
+        .attr("dy", ".5em")
+        .text(function (d) { return d.x});
 })
 
-function processDataType(d) {
+function processDataType(d){
     d.value = +d.value; //grabbing the VALUE
     return d;
 }
